98f5a1bfd9fca1e0ffd10eb318b078aa9c27a1e2
alanb-02 <mathew02alan@gmail.com>
2022-11-14 16:39:41 +0000

    Worksheet 3 Problem 1: not can now be added and colour selected

--- worksheet3/problem-1/problem-1.js
+++ worksheet3/problem-1/problem-1.js
@@ -3,23 +3,45 @@ var add = document.getElementById("add");
 
 
 const addNewNote = {
-    next: function() {
+  next: function() {
 
+/*     console.log("hola"); */
+    noteNum++;
+
+    const note = document.createElement('div');
+    note.setAttribute("id", "note-" + noteNum);
+    note.setAttribute("class", "note");
+
+    const toolbar = document.createElement('div');
+    toolbar.setAttribute("class", "tools");
+    toolbar.innerHTML =  `
+      <button class="edit" style="background-color:green">Edit/Save</button>  
+      <button class="delete" style="background-color:red">Delete</button>`;
+    note.appendChild(toolbar);
+
+    const notesText = document.createElement('textarea');
+    note.appendChild(notesText);
+
+    var noteColourBtn = document.getElementById('colours');
+    const edSaveBtn = document.getElementsByClassName('edit');
+    const delBtn = document.getElementsByClassName('delete'); 
+
+    document.body.appendChild(note);
+
+    notesText.style.backgroundColor = noteColourBtn.value;
+
+
+/*     const changeColor = {
+      next: function() {
         console.log("hola");
-        noteNum++;
-        let note = document.createElement('div');
-        note.setAttribute("id", "note-" + noteNum);
-
-        const notesText = document.createElement("textarea");
-        note.appendChild(notesText);
-
-        document.body.appendChild(note);
-
-
-
-
-        return note.id;
+      }
     }
+    var observableCol = Rx.Observable.fromEvent(noteColourBtn, 'click');
+    observableCol.subscribe(changeColor); */
+
+
+    return note.id;
+  }
 }
 
 var addObservable = Rx.Observable.fromEvent(add, 'click');
