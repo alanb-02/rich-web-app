d93c0a386300f0e11255a362696f429b8800d5a9
alanb-02 <mathew02alan@gmail.com>
2022-11-21 16:55:21 +0000

    fixed multiple go bug

--- worksheet3/problem-2/timer.js
+++ worksheet3/problem-2/timer.js
@@ -3,49 +3,57 @@ var displayM = document.getElementById("minute");
 var dispalyS = document.getElementById("second");
 const startBtn = document.getElementById("startBtn");
 var count = 0;
+var running = false;
 
 const timeInput = {
     next: function() {
-        const h = document.getElementById('inputHour').value;
-        const m = document.getElementById('inputMinute').value;
-        const s = document.getElementById('inputSecond').value;
 
-        var hTos = h * 3600;
-        var mTos = m * 60;
-        var duration = hTos + mTos + parseInt(s);
-        /* console.log(hTos, mTos, sTos, duration); */
-
-        var startCount = Rx.Observable.interval(1000).map(count => duration - count).subscribe(seconds => {
-            var time = String(seconds/3600);
-            /* console.log(time);  */
-
-            hrs = seconds / 3600;
-            hrs = Math.trunc(h);
-
-            mins = seconds % 3600;
-            mins = mins / 60;
-            mins = Math.trunc(mins);
-
-            secs = seconds % 3600;
-            secs = secs % 60;
-
-            if ( hrs < 10 || mins < 10  || secs < 10) {
-                hrs = String(hrs).padStart(2, '0');
-                mins = String(mins).padStart(2, '0');
-                secs = String(secs).padStart(2, '0');
-            }
-
-            displayH.innerHTML = hrs;
-            displayM.innerHTML = mins;
-            dispalyS.innerHTML = secs
-
-            console.log(hrs, mins, secs);  
-
-            if(time == 0) {
-                startCount.unsubscribe();
+        if (running === true) {
+            alert("Wait for CountDown to Finish !!!")
+        }else {
+            const h = document.getElementById('inputHour').value;
+            const m = document.getElementById('inputMinute').value;
+            const s = document.getElementById('inputSecond').value;
+    
+            var hTos = h * 3600;
+            var mTos = m * 60;
+            var duration = hTos + mTos + parseInt(s);
+            /* console.log(hTos, mTos, sTos, duration); */
+    
+            var startCount = Rx.Observable.interval(1000).map(count => duration - count).subscribe(seconds => {
+                var time = String(seconds/3600);
+                /* console.log(time);  */
+    
+                hrs = seconds / 3600;
+                hrs = Math.trunc(h);
+    
+                mins = seconds % 3600;
+                mins = mins / 60;
+                mins = Math.trunc(mins);
+    
+                secs = seconds % 3600;
+                secs = secs % 60;
+    
+                if ( hrs < 10 || mins < 10  || secs < 10) {
+                    hrs = String(hrs).padStart(2, '0');
+                    mins = String(mins).padStart(2, '0');
+                    secs = String(secs).padStart(2, '0');
+                }
+    
+                displayH.innerHTML = hrs;
+                displayM.innerHTML = mins;
+                dispalyS.innerHTML = secs
+    
+                console.log(hrs, mins, secs);  
+                running = true;
                 
-            }
-        }) 
+                if(time == 0) {
+                    startCount.unsubscribe();
+                    running = false;
+                }
+                
+            }) 
+        }
     }
 }
 
