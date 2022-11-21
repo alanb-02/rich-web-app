db62f4609510a3f29080778f9a93640a97b20ec6
alanb-02 <mathew02alan@gmail.com>
2022-11-21 15:02:11 +0000

    able to countdown, and convert the seconds back h and m to display

--- worksheet3/problem-2/timer.js
+++ worksheet3/problem-2/timer.js
@@ -1,6 +1,6 @@
 var displayH = document.getElementById("hour");
 var displayM = document.getElementById("minute");
-var dispalyS = document.getElementById("seconds");
+var dispalyS = document.getElementById("second");
 const startBtn = document.getElementById("startBtn");
 var count = 0;
 
@@ -15,7 +15,36 @@ const timeInput = {
         var duration = hTos + mTos + parseInt(s);
         /* console.log(hTos, mTos, sTos, duration); */
 
-        
+        var startCount = Rx.Observable.interval(1000).map(count => duration - count).subscribe(seconds => {
+            var time = String(seconds/3600);
+            /* console.log(time);  */
+
+            hrs = seconds / 3600;
+            hrs = Math.trunc(h);
+
+            mins = seconds % 3600;
+            mins = mins / 60;
+            mins = Math.trunc(mins);
+
+            secs = seconds % 3600;
+            secs = secs % 60;
+
+            console.log(hrs, mins, secs); 
+
+            displayH.innerHTML = hrs;
+            displayM.innerHTML = mins;
+            dispalyS.innerHTML = secs
+
+
+
+            /* var date = new Date(0, 0); */
+            /* date.setSeconds(+time * 60 * 60);
+            console.log(date); */
+            if(time == 0) {
+                startCount.unsubscribe();
+                
+            }
+        }) 
     }
 }
 
