4629bcf675542911102588a6e91746a4c6f8ed80
alanb-02 <mathew02alan@gmail.com>
2022-11-23 00:02:18 +0000

    retrying the add sub note part with classes

--- worksheet3/problem-3/problem-1.js
+++ worksheet3/problem-3/problem-1.js
@@ -1,7 +1,12 @@
 let noteNum = 0;
 let noteFull = 0;
-let subNoteNum = 0;
 var add = document.getElementById("add");
+/* const notes = JSON.parse(localStorage.getItem('notes')) 
+
+
+if(notes) {  
+  notes.forEach(note => addNewNote(note))  
+} */
 
 
 const addNewNote = {
@@ -15,17 +20,17 @@ const addNewNote = {
       const note = document.createElement('div');
       note.setAttribute("id", "note-" + noteNum);
       note.setAttribute("class", "note");
-  
+
       const toolbar = document.createElement('div');
       toolbar.setAttribute("class", "tools");
-   
+
       const delBtn = document.createElement('button');
       delBtn.innerHTML = "Delete";
       delBtn.setAttribute("id", "note-" + noteNum);
       delBtn.setAttribute("class", "delete");
       delBtn.setAttribute("style", "background-color:red");
       toolbar.appendChild(delBtn)
-  
+
       const edSaveBtn = document.createElement('button');
       edSaveBtn.innerHTML = "Save";
       edSaveBtn.setAttribute("id", "note-" + noteNum);
@@ -35,36 +40,24 @@ const addNewNote = {
 
       const addSubBtn = document.createElement('button');
       addSubBtn.innerHTML = "Add Sub Note";
+      addSubBtn.className = "edit"
       addSubBtn.setAttribute("id", "note-" + noteNum);
-      addSubBtn.setAttribute("class", "addSub");
+      /* addSubBtn.setAttribute("class", "addSub"); */
       addSubBtn.setAttribute("style", "background-color:#F1C232");
       toolbar.appendChild(addSubBtn)
-  
+
       note.appendChild(toolbar);
-  
+
       const textArea = document.createElement('textarea');
       textArea.readOnly = false;
       note.appendChild(textArea);
 
-
-      const toolbar2 = document.createElement('div');
-      toolbar2.setAttribute("class", "tools");
-      note.appendChild(toolbar2);
-
-      const delSubBtn = document.createElement('button');
-      delSubBtn.innerHTML = "Delete";
-      delSubBtn.setAttribute("id", "Note-" + noteNum);
-      delSubBtn.setAttribute("class", "delete");
-      delSubBtn.setAttribute("value", subNoteNum);
-      delSubBtn.setAttribute("style", "background-color:red");
-      toolbar2.appendChild(delSubBtn)
-  
       var noteColourBtn = document.getElementById('colours');
-  
+
       document.body.appendChild(note);
-  
+
       textArea.style.backgroundColor = noteColourBtn.value;
-  
+
       const deleteNote = {
         next: function(){
           document.getElementById(delBtn.getAttribute("id")).remove();
@@ -73,7 +66,7 @@ const addNewNote = {
       }
       var delObservable = Rx.Observable.fromEvent(delBtn, 'click');
       delObservable.subscribe(deleteNote);
-  
+
       const edSaveNote = {
         next: function() {
           if (textArea.readOnly == false){
@@ -84,41 +77,22 @@ const addNewNote = {
             edSaveBtn.innerHTML = "Save";
           }
           alert(textArea.value)
-          /* textArea.readOnly = true; */
+
           note.appendChild(textArea);
         }
       }
       var edObservable = Rx.Observable.fromEvent(edSaveBtn, 'click');
       edObservable.subscribe(edSaveNote);
 
-      const adSubNote = {
+      const addNewSubNote = {
         next: function() {
-          const subTextDiv = document.createElement('div');
-          subTextDiv.setAttribute("class", "snote-" + noteNum);
-          subTextDiv.setAttribute("value", subNoteNum);
-          subTextDiv.setAttribute("id", "Note-" + noteNum);
-          note.appendChild(subTextDiv);
-
-          const subTextArea = document.createElement('textarea');
-          subTextDiv.appendChild(subTextArea);
-
-          const deleteSubNote = {
-            next: function(){
-              document.getElementById(delSubBtn.getAttribute("value")).remove();
-              noteFull--;
-            }/* =========================================== delete button va;ue */
-          }
-          var delSubObservable = Rx.Observable.fromEvent(delSubBtn, 'click');
-          delSubObservable.subscribe(deleteSubNote);
-
-          subNoteNum++;
+          const childNote = document.createElement("textarea");
+          note.appendChild(childNote);
         }
       }
-      var adsubObservable = Rx.Observable.fromEvent(addSubBtn, 'click');
-      adsubObservable.subscribe(adSubNote);
+      var addSObservable = Rx.Observable.fromEvent(addNewSubNote, 'click');
+      addSObservable.subscribe(addNewSubNote);
 
-      
-  
       return note.id;
     }
   }
@@ -127,3 +101,10 @@ const addNewNote = {
 var addObservable = Rx.Observable.fromEvent(add, 'click');
 addObservable.subscribe(addNewNote);
 
+
+/* function updateLS() {  
+  const notesText = document.querySelectorAll('textarea')  
+  const notes = []  
+  notesText.forEach(note => notes.push(note.value))  
+  localStorage.setItem('notes', JSON.stringify(notes))  
+}   */
