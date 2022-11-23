e353ef2882172dfa0e1058db7a3699fc8a7d5648
alanb-02 <mathew02alan@gmail.com>
2022-11-23 00:46:40 +0000

    adding subnote works, delete still does not work as I am using remove

--- worksheet3/problem-3/problem-1.js
+++ worksheet3/problem-3/problem-1.js
@@ -11,90 +11,87 @@ if(notes) {
 
 const addNewNote = {
   next: function() {
+    noteNum++;
+    noteFull++;
+    const note = document.createElement('div');
+    note.setAttribute("id", "note-" + noteNum);
+    note.setAttribute("class", "note");
 
-    if (noteFull === 3){
-      alert("Notepad Full!!");
-    }else {
-      noteNum++;
-      noteFull++;
-      const note = document.createElement('div');
-      note.setAttribute("id", "note-" + noteNum);
-      note.setAttribute("class", "note");
+    const toolbar = document.createElement('div');
+    toolbar.setAttribute("class", "tools");
 
-      const toolbar = document.createElement('div');
-      toolbar.setAttribute("class", "tools");
+    const delBtn = document.createElement('button');
+    delBtn.innerHTML = "Delete";
+    delBtn.setAttribute("id", "note-" + noteNum);
+    delBtn.setAttribute("class", "delete");
+    delBtn.setAttribute("style", "background-color:red");
+    toolbar.appendChild(delBtn)
 
-      const delBtn = document.createElement('button');
-      delBtn.innerHTML = "Delete";
-      delBtn.setAttribute("id", "note-" + noteNum);
-      delBtn.setAttribute("class", "delete");
-      delBtn.setAttribute("style", "background-color:red");
-      toolbar.appendChild(delBtn)
+    const edSaveBtn = document.createElement('button');
+    edSaveBtn.innerHTML = "Save";
+    edSaveBtn.setAttribute("id", "note-" + noteNum);
+    edSaveBtn.setAttribute("class", "edit");
+    edSaveBtn.setAttribute("style", "background-color:green");
+    toolbar.appendChild(edSaveBtn)
 
-      const edSaveBtn = document.createElement('button');
-      edSaveBtn.innerHTML = "Save";
-      edSaveBtn.setAttribute("id", "note-" + noteNum);
-      edSaveBtn.setAttribute("class", "edit");
-      edSaveBtn.setAttribute("style", "background-color:green");
-      toolbar.appendChild(edSaveBtn)
+    const addSubBtn = document.createElement('button');
+    addSubBtn.innerHTML = "Add Sub Note";
+    addSubBtn.className = "edit"
+    addSubBtn.setAttribute("id", "note-" + noteNum);
+    /* addSubBtn.setAttribute("class", "addSub"); */
+    addSubBtn.setAttribute("style", "background-color:#F1C232");
+    toolbar.appendChild(addSubBtn)
 
-      const addSubBtn = document.createElement('button');
-      addSubBtn.innerHTML = "Add Sub Note";
-      addSubBtn.className = "edit"
-      addSubBtn.setAttribute("id", "note-" + noteNum);
-      /* addSubBtn.setAttribute("class", "addSub"); */
-      addSubBtn.setAttribute("style", "background-color:#F1C232");
-      toolbar.appendChild(addSubBtn)
+    note.appendChild(toolbar);
 
-      note.appendChild(toolbar);
+    const textArea = document.createElement('textarea');
+    textArea.readOnly = false;
+    note.appendChild(textArea);
+    
 
-      const textArea = document.createElement('textarea');
-      textArea.readOnly = false;
-      note.appendChild(textArea);
+    var noteColourBtn = document.getElementById('colours');
 
-      var noteColourBtn = document.getElementById('colours');
+    document.body.appendChild(note);
 
-      document.body.appendChild(note);
+    textArea.style.backgroundColor = noteColourBtn.value;
 
-      textArea.style.backgroundColor = noteColourBtn.value;
-
-      const deleteNote = {
-        next: function(){
-          document.getElementById(delBtn.getAttribute("id")).remove();
-          noteFull--;
-        }
+    const deleteNote = {
+      next: function(){
+        document.getElementById(delBtn.getAttribute("id")).remove();
       }
-      var delObservable = Rx.Observable.fromEvent(delBtn, 'click');
-      delObservable.subscribe(deleteNote);
-
-      const edSaveNote = {
-        next: function() {
-          if (textArea.readOnly == false){
-            textArea.readOnly = true;
-            edSaveBtn.innerHTML = "Edit";
-          }else {
-            textArea.readOnly = false;
-            edSaveBtn.innerHTML = "Save";
-          }
-          alert(textArea.value)
-
-          note.appendChild(textArea);
-        }
-      }
-      var edObservable = Rx.Observable.fromEvent(edSaveBtn, 'click');
-      edObservable.subscribe(edSaveNote);
-
-      const addNewSubNote = {
-        next: function() {
-          const childNote = document.createElement("textarea");
-          note.appendChild(childNote);
-        }
-      }
-      var addSObservable = Rx.Observable.fromEvent(addNewSubNote, 'click');
-      addSObservable.subscribe(addNewSubNote);
-
-      return note.id;
     }
+    var delObservable = Rx.Observable.fromEvent(delBtn, 'click');
+    delObservable.subscribe(deleteNote);
+
+    const edSaveNote = {
+      next: function() {
+        if (textArea.readOnly == false){
+          textArea.readOnly = true;
+          edSaveBtn.innerHTML = "Edit";
+        }else {
+          textArea.readOnly = false;
+          edSaveBtn.innerHTML = "Save";
+        }
+        alert(textArea.value)
+
+        note.appendChild(textArea);
+      }
+    }
+    var edObservable = Rx.Observable.fromEvent(edSaveBtn, 'click');
+    edObservable.subscribe(edSaveNote);
+
+    const addNewSubNote = {
+      next: function() {
+        const childNote = document.createElement("textarea");
+        childNote.setAttribute("id", "subNote");
+        childNote.setAttribute("style", "background-color:#595959; color:white");
+        note.appendChild(childNote);
+      }
+    }
+    var addSObservable = Rx.Observable.fromEvent(addSubBtn, 'click');
+    addSObservable.subscribe(addNewSubNote);
+
+    return note.id;
   }
 }
 
