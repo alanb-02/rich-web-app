cdcbf26d2e15dce5e75495fdb4a4a37903ac4746
alanb-02 <mathew02alan@gmail.com>
2022-12-18 18:45:59 +0000

    update app js with header and add note component

--- worksheet4/notes-app/src/App.js
+++ worksheet4/notes-app/src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { NotFoundError } from 'rxjs';
+import { ReactDOM } from 'react';
+import React from 'react';
+import Note from './Note';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Notepad</h2>
+        <Note/>
       </header>
     </div>
   );
